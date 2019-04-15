7d40aa98412030cb0919b1bb2b65f16d45d57cb8
Anh Tuan <ntduong.mnk6c@hnmu.edu.vn>
2019-04-15 21:44:36 +0700

    --  Register form customer

---
+++ Mobie_store/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mobie_store.Models.Entity;
+
+namespace Mobie_store.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Product(int id)
+        {
+            MyDBContext db = new MyDBContext();
+            List<product> product = db.products.Where(m => m.product_cate_id == id).ToList();
+            return View(product);
+        }
+    }
+}
