d7f09b54f3728954086ee181a305d1b774253e27
Anh Tuan <ntduong.mnk6c@hnmu.edu.vn>
2019-05-07 13:15:44 +0700

    -- repair some error

--- Mobie_store/Controllers/DetailController.cs
+++ Mobie_store/Controllers/DetailController.cs
@@ -12,11 +12,23 @@ namespace Mobie_store.Controllers
 {
     public class DetailController : Controller
     {
+        public static List<image> getorder(int? id)
+        {
+            using (var db = SetupConnection.ConnectionFactory())
+            {
+                if (db.State == ConnectionState.Closed)
+                    db.Open();
+                var sql = "SELECT * FROM dbo.image Where product_id =@id;";
+                return db.Query<image>(sql, new { id = id }).ToList();
+            }
+        }
         // GET: Detail
         public ActionResult Index(int id)
         {
             MyDBContext db = new MyDBContext();
             product product = db.products.Find(id);
+            List<image> img = DetailController.getorder(id);
+            ViewBag.Img = img;
             return View(product);
         }
     }
